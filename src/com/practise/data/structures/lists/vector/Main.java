c9b92a1aa9b011c07132b1fb15e3f9768b2cf8e5
lavanya <ml.lavanya2000@gmail.com>
2024-09-28 23:21:06 -0700

    added vector data structure examples

--- src/com/practise/data/structures/lists/vector/Main.java
+++ src/com/practise/data/structures/lists/vector/Main.java
@@ -1,14 +1,17 @@
-package com.practise.data.structures.lists.arraylist;
+package com.practise.data.structures.lists.vector;
 
 import com.practise.data.structures.common.Employee;
 
-import java.util.ArrayList;
+import java.util.Vector;
 import java.util.List;
 
 public class Main {
+    /*
+    *Vector is same as Array list except it is Syncronized.
+    */
     public static void main(String[] args) {
-        List<Employee> employeeList = new ArrayList<>();
-        System.out.println("\n************** Array list add method ***************");
+        List<Employee> employeeList = new Vector<>();
+        System.out.println("\n************** Vector add method ***************");
         employeeList.add(new Employee("bob", "jacob", 123));
         employeeList.add(new Employee("john", "doe", 124));
         employeeList.add(new Employee("mike", "steve", 125));
@@ -18,7 +21,7 @@ public class Main {
             System.out.println(emp);
         }
 
-        System.out.println("\n************** Array list set method ***************");
+        System.out.println("\n************** Vector set method ***************");
         employeeList.set(1, new Employee("alex", "david", 126));
         // To print all employees using list.foreach and lambda function
         employeeList.forEach(System.out::println);
@@ -26,16 +29,16 @@ public class Main {
         System.out.println("Employee list size" + employeeList.size());
 
         // convert list to array
-        System.out.println("\n************** Array list toArray method ***************");
+        System.out.println("\n************** Vector toArray method ***************");
         Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
         for(Employee emp: employeeArray){
             System.out.println(emp);
         }
         // look for a particular item in list
-        System.out.println("\n************** Array list indexOf method ***************");
+        System.out.println("\n************** Vector indexOf method ***************");
         System.out.println(employeeList.indexOf(new Employee("bob", "jacob", 123)));
 
-        System.out.println("\n************** Array list remove method ***************");
+        System.out.println("\n************** Vector remove method ***************");
         employeeList.remove(2);
         employeeList.forEach(System.out::println);
     }
